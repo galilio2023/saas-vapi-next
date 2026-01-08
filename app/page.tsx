67b01f821d0502896a793283ad9af873a9c3e0bd
ibrahim galal <ibrahimgalal2011@gmail.com>
2026-01-08 23:12:03 +0200

    test gemeni

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 const Page = () => {
+  const x = 1;
   return (
     <div>
       <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
