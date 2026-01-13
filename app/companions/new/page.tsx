775395fb3980852c5b0063f24bf646bff09adfc3
ibrahim galal <ibrahimgalal2011@gmail.com>
2026-01-13 16:17:58 +0200

    feat:Implement form ui to submit the companions

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import CompanionForm from "@/components/CompanionForm";
 
 const NewCompanion = () => {
-  return <div>NewCompanion</div>;
+  return (
+    <main className="items-center justify-center md:w-2/3 lg:w-1/3">
+      <article className="flex w-full flex-col gap-4">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
+  );
 };
 export default NewCompanion;
