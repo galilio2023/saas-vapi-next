b050cd81d5bbdf4c9dfd8543d814af8edde66ec8
ibrahim galal <ibrahimgalal2011@gmail.com>
2026-01-14 22:03:53 +0200

    feat:Implemet DB supabse and integrated with clerk and implment create companions with server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
   return (
     <main className="items-center justify-center md:w-2/3 lg:w-1/3">
       <article className="flex w-full flex-col gap-4">
